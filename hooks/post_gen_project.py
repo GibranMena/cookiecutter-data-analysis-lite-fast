000328389876829e7ac7cc045c30aace1485b152
Fer Aguirre <77854667+fer-aguirre@users.noreply.github.com>
2025-04-01 23:18:01 -0600

    Added package mode setting for poetry

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -15,35 +15,35 @@ if package_manager == "poetry":
     print(f"{MESSAGE_COLOR}Installing Poetry...{RESET_ALL}")
     os.system("pipx install poetry")
 
-    # Create pyproject.toml
+    # Create pyproject.toml - Note the removal of indentation
     print(f"{MESSAGE_COLOR}Creating pyproject.toml file...{RESET_ALL}")
     with open("pyproject.toml", "w") as f:
         f.write("""[tool.poetry]
-                    name = "{{cookiecutter.project_slug}}"
-                    version = "0.1.0"
-                    description = "{{cookiecutter.project_description}}"
-                    authors = ["{{cookiecutter.project_author}}"]
-                    readme = "README.md"
+name = "{{cookiecutter.project_slug}}"
+version = "0.1.0"
+description = "{{cookiecutter.project_description}}"
+authors = ["{{cookiecutter.project_author}}"]
+readme = "README.md"
+package-mode = false
 
-                    [tool.poetry.dependencies]
-                    python = "^3.8"
-                    ipykernel = "^6.0.3"
-                    nbformat = "^5.1.3"
-                    pandas = "^1.3.0"
-                    numpy = "^1.21.0"
-                    requests = "^2.26.0"
-                    plotly = "^5.3.1"
-                    openpyxl = "^3.0.9"
+[tool.poetry.dependencies]
+python = "^3.8"
+ipykernel = "^6.0.3"
+nbformat = "^5.1.3"
+pandas = "^1.3.0"
+numpy = "^1.21.0"
+requests = "^2.26.0"
+plotly = "^5.3.1"
+openpyxl = "^3.0.9"
 
-                    [build-system]
-                    requires = ["poetry-core"]
-                    build-backend = "poetry.core.masonry.api"
-                    """)
-    
+[build-system]
+requires = ["poetry-core"]
+build-backend = "poetry.core.masonry.api"
+""")
     
     # Install virtual environment and synchronize packages
     print(f"{MESSAGE_COLOR}Creating virtual environment with Poetry...{RESET_ALL}")
-    os.system(f"poetry env use python{{cookiecutter.python_version}}")
+    os.system(f"poetry env use python{{{cookiecutter.python_version}}}")
     os.system("poetry install")
     
 elif package_manager == "uv":
@@ -53,7 +53,7 @@ elif package_manager == "uv":
     
     # Set up virtual environment with uv
     print(f"{MESSAGE_COLOR}Creating virtual environment with uv...{RESET_ALL}")
-    os.system(f"uv venv --python={{cookiecutter.python_version}}")
+    os.system(f"uv venv --python={{{cookiecutter.python_version}}}")
     os.system("uv pip install -r requirements.txt")
 
 # Initialize git (common for both package managers)
